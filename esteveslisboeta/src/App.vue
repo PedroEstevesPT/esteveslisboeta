6cc35950d7d68a640b485358a999f5410845f7f6
Pedro Esteves <pedrofonsecaesteves@gmail.com>
2025-05-11 20:14:29 +0100

    changed code to attempt to prevent horizontal scroll in mobile

--- esteveslisboeta/src/App.vue
+++ esteveslisboeta/src/App.vue
@@ -9,16 +9,17 @@
   import TopNav from './components/TopNav.vue'
   </script>
   
-  <style>
-  /* Ensure the body element takes up 100% height and has no scrolling */
-  body, html {
-    margin: 0;
-    padding: 0;
-    height: 100%;
-  }
-  
-  /* Ensure the root element takes up the full viewport height */
-  #app {
-    height: 100vh; /* Occupy full height of the viewport */
-  }
-  </style>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html, body {
+  overflow-x: hidden;
+  width: 100%;
+}
+
+#app {
+  overflow-x: hidden;
+}
+</style>
