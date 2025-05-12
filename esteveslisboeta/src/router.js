551c42eba9f98c75d11532dfe03d71537bcea8be
Pedro Esteves <pedrofonsecaesteves@gmail.com>
2025-05-12 23:32:13 +0100

    replaced webhashhistory with webhistory

--- esteveslisboeta/src/router.js
+++ esteveslisboeta/src/router.js
@@ -1,16 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router' // Change here
 import Landing from './views/LandingPage.vue'
 import Collab from './views/CollabPage.vue'
 import Discography from './views/DiscographyPage.vue'
 
 const routes = [
-  { path: '/', component: Landing ,                name : 'Landing'                },
-  { path: '/collab', component: Collab,            name: 'Collab'          },
+  { path: '/', component: Landing ,                name : 'Landing' },
+  { path: '/collab', component: Collab,            name: 'Collab' },
   { path: '/discography', component: Discography,  name: 'Discography' }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // Change here
   routes
 })
 
